09211ca645948fbfa12de855d15cce622a58410b
lorenzo91347 <lorenzo.losito91@gmail.com>
2024-10-31 13:11:20 +0100

    Initialize project using Create React App

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './main.css';
+
+const Booklist = () => {
+    return <section className='booklist'>
+        <Book />
+        <Book />
+        <Book />
+    </section>
+}
+
+
+const Book = () => {
+    return (
+        <article className='book'>
+            <Image />
+            <Title />
+            <Author />
+        </article>
+    );
+};
+
+const Image = () => <img src='https://images-na.ssl-images-amazon.com/images/I/41e+dwz5W5L._AC_UL127_SR127,127_.jpg' alt='Melania' />
+const Title = () => <h2>Melania</h2>
+const Author = () => {
+    return <h4>Melania Trump</h4>
+}
+
+
+/* function Greeting() {
+    return React.createElement('h2', {}, 'hello world')
+}; */
+
+/* const Greeting = () => {
+    return React.createElement(
+        'div',
+        {},
+        React.createElement('h2', {}, 'hello world')
+    );
+}; */
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Booklist />);
+export default Booklist; 
